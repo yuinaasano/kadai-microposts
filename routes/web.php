fe093d3dbe16b223819f3e51b245af48bddabe0e
yuina.asano <yuina.asano@rakuten.com>
2018-06-12 04:40:19 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function () {
+Route::get('/', 'MicropostsController@index');
+
+// omit
+
+Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
